4f82641315cf1f24db0379f8bd98bd55bb9be275
Matheus Azambuja <matheus.azambuja@foregon.com>
2019-04-21 01:26:04 -0300

    change ComicsListPage to use hook

--- src/components/ComicsListPage/index.js
+++ src/components/ComicsListPage/index.js
@@ -1,13 +1,11 @@
-import React, { Component, Fragment } from 'react'
+import React from 'react'
 import ComicsList from '../ComicsList'
 
-export default class ComicsListPage extends Component {
-  render() {
-    return (
-      <Fragment>
-        <h1>Comics List</h1>
-        <ComicsList/>
-      </Fragment>
-    )
-  }
+export default function ComicsListPage() {
+  return (
+    <>
+      <h1>Comics List</h1>
+      <ComicsList/>
+    </>
+  )
 }
